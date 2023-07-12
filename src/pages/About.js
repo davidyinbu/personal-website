64f7af2d3317b4d388bbcd2c9db034396d617e6a
DaweiYin <davidyin@bu.edu>
2023-07-11 22:09:13 -0400

    fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,42 +8,33 @@ const About = () => {
   const [markdown, setMarkdown] = useState('');
 
   useEffect(() => {
-    import('../data/about.md')
-      .then((res) => {
-        fetch(res.default)
-          .then((r) => r.text())
-          .then(setMarkdown);
-      });
+    import('../data/about.md').then((res) => {
+      fetch(res.default)
+        .then((r) => r.text())
+        .then(setMarkdown);
+    });
   });
 
-  const count = markdown.split(/\s+/)
+  const count = markdown
+    .split(/\s+/)
     .map((s) => s.replace(/\W/g, ''))
     .filter((s) => s.length).length;
 
-// Make all hrefs react router links
-const LinkRenderer = ({ ...children }) => <Link {...children} />;
-
-const About = () => (
-  <Main
-    title="About"
-    description="Learn about Dawei Yin"
-  >
-    <article className="post markdown" id="about">
-      <header>
-        <div className="title">
-          <h2 data-testid="heading"><Link to="/about">About Me</Link></h2>
-          <p>(in about {count} words)</p>
-        </div>
-      </header>
-      <ReactMarkdown
-        source={markdown}
-        renderers={{
-          Link: LinkRenderer,
-        }}
-        escapeHtml={false}
-      />
-    </article>
-  </Main>
-);
+  return (
+    <Main title="About" description="Learn about Me">
+      <article className="post markdown" id="about">
+        <header>
+          <div className="title">
+            <h2>
+              <Link to="/about">About Me</Link>
+            </h2>
+            <p>(in about {count} words)</p>
+          </div>
+        </header>
+        <Markdown>{markdown}</Markdown>
+      </article>
+    </Main>
+  );
+};
 
 export default About;
